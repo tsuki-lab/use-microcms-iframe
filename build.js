de1354b9278f8831cec5be83600973f42328e525
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-05-05 01:27:06 +0900

    Update version v0.1.18

---
+++ build.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { build } = require('esbuild')
+const { dependencies } = require('./package.json')
+
+const entryFile = 'src/index.ts'
+const shared = {
+  bundle: true,
+  entryPoints: [entryFile],
+  external: Object.keys(dependencies),
+  logLevel: 'info',
+  minify: true,
+  sourcemap: false,
+}
+
+build({
+  ...shared,
+  format: 'esm',
+  outfile: './dist/index.esm.js',
+  target: ['ES6'],
+})
+
+build({
+  ...shared,
+  format: 'cjs',
+  outfile: './dist/index.cjs.js',
+  target: ['ES6'],
+})
